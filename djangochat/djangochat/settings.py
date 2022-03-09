15d03b4418d32e245bd09f28536f3909ac510097
Sai Charan Reddy <dvsaicharandvs@gmail.com>
2022-03-09 16:28:07 +0530

    third commit

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,4 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
